deeefdce965daffb8d3709387a39a4d40d3fe9e4
Negrativo <49249175+Negrativo@users.noreply.github.com>
2021-05-29 00:23:44 -0300

    Implementado os favoritos do user

--- backend/src/model/Favoritos.js
+++ backend/src/model/Favoritos.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
+const Empresa = require("../model/Empresa");
 
 //Schema -> tabela de empresa no Banco
 const FavoritoSchema = new mongoose.Schema({ 
     nnumeuser     : {
         type: mongoose.Schema.Types.ObjectId,
         ref : 'User'
+    },
+    nnumeempr     : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : 'Empresa'
     }
+
 });
 
 
